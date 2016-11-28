4d12315023b15aa9ccdba7ff34a6a3307a48d6b8
Forrest Chai <151905Y@mymail.nyp.edu.sg>
2016-11-28 17:26:49 +0800

    Initial commit

---
+++ src/demo/WebSocketClock.java
@@ -0,0 +1,39 @@
+package demo;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@ServerEndpoint("/clock")
+public class WebSocketClock {
+    static ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
+    DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+    @OnOpen
+    public void showTime(Session session){
+        System.out.println("Opened : " + session.getId());
+        timer.scheduleAtFixedRate(
+                () -> sendTime(session), 0, 1, TimeUnit.SECONDS);
+    }
+
+    private void sendTime(Session session){
+        try{
+            String data = "Time: " + LocalTime.now().format(timeFormatter);
+            session.getBasicRemote().sendText(data);
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+
+    }
+    @OnMessage
+    public void onMessage(String txt, Session session) throws IOException{
+        System.out.println("Message recieved "+txt);
+        session.getBasicRemote().sendText(txt);
+    }
+}
